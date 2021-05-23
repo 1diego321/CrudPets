d0cb0f6d63388d78ed686c3e255587c3de57a85b
Luis Diego Solis Camacho <1diego321@gmail.com>
2021-05-23 10:15:02 -0600

    SetOf3layersArchitecture

---
+++ ApiCrudPets.DAL/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiCrudPets.DAL.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> AppUser { get; set; }
+        public DbSet<Owner> Owner { get; set; }
+        public DbSet<Pet> Pet { get; set; }
+    }
+}
--- ApiCrudPets.DAL/Entities/AppUser.cs
+++ ApiCrudPets.DAL/Entities/AppUser.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ApiCrudPets.Models
+namespace ApiCrudPets.DAL.Entities
 {
     public class AppUser
     {
